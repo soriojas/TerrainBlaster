656854cfa9552ea00b7ee24c48e39604bfbba26e
soriojas <32626362+soriojas@users.noreply.github.com>
2022-07-07 21:01:27 -0400

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 10.0f;
+
     // Start is called before the first frame update
     void Start()
-    {
-        
+    { 
+        float translation = Input.GetAxis("Vertical") * speed;
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    { 
+        transform.Translate(0, 0, translation);
     }
 }
 //
